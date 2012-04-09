b2699c4a8eaedc54cdeefde2ab6ea01392ab2cf7
Keith Yao <i@yaofur.com>
2012-04-09 19:41:30 +0800

    update torrent_edit

---
+++ static/js/editor.coffee
@@ -0,0 +1,5 @@
+$ ->
+  ###_______###
+  #
+  ###_______###
+  editor = new EpicEditor( $('#input-descr').get(0) ).load()
---
+++ static/js/editor.js
@@ -0,0 +1,12 @@
+(function() {
+
+  $(function() {
+    /*_______
+    */
+    /*_______
+    */
+    var editor;
+    return editor = new EpicEditor($('#input-descr').get(0)).load();
+  });
+
+}).call(this);
